d285434c38a39a4c098dc6bc0c36cd625c3a2ca7
TracyTN <tracytn0411@gmail.com>
2019-09-19 14:32:44 -0700

    Create new table (departments).

--- bamazon.sql
+++ bamazon.sql
@@ -19,8 +19,27 @@ VALUES 	('Apple Macbook Air', 'Laptops', 1500.75, 20),
 		    ('Google Pixel 4', 'Smart Phones', 749.00, 30),
 		    ('Apple Iphone 11', 'Smart Phones', 815.25, 25),
 		    ('Samsung Galaxy S10', 'Smart Phones', 1200.00, 25),
-		    ('Goole Pixel Watch', 'Fitness Trackers', 400.75, 20),
+		    ('Google Pixel Watch', 'Fitness Trackers', 400.75, 20),
 		    ('Fitbit Inspire HR', 'Fitness Trackers', 99.95, 50),
 		    ('Nintendo Switch', 'Video Games', 299.99, 15),
 		    ('PlayStation 4', 'Video Games', 275.25, 20),
 		    ('XBox ONE', 'Video Games', 350.50, 15) ;
+
+
+-- Add column product_sales to the table
+ALTER TABLE products
+ADD product_sales DECIMAL(7,2) NOT NULL;
+
+-- Create new table for supervisors
+CREATE TABLE departments (
+  department_id     INT(11) NOT NULL AUTO_INCREMENT,
+  department_name   VARCHAR(255) NOT NULL,
+  over_head_cost    DECIMAL(7,2) NOT NULL,
+  PRIMARY KEY       (department_id)
+);
+
+INSERT INTO departments (department_name, over_head_cost)
+VALUES ('Laptops', 5000),
+		('Smart Phones', 3000),
+		('Fitness Trackers', 1000),
+		('Video Games', 2000);
